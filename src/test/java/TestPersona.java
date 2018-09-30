f134e48885643032e84f3595ea58842628dd5a9a
rdavidnota <rdavid.nota@gmail.com>
2018-09-30 06:30:26 -0400

    add test de pruebai

---
+++ src/test/java/TestPersona.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by rdavi on 30/09/2018.
+ */
+public class TestPersona {
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+        assertTrue(true);
+    }
+}
